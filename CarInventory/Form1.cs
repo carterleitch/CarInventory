5b769234e7c665fa9fdfd2efa68655aea4fb38a4
Tony Theodoropoulos <theodot@outlook.com>
2019-02-19 10:48:11 -0500

    Add project files.

---
+++ CarInventory/Form1.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace CarInventory
+{
+    public partial class Form1 : Form
+    {
+        List<Car> cars = new List<Car>();
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void addButton_Click(object sender, EventArgs e)
+        {
+            string make, colour;
+            int year, mileage;
+
+            make = makeInput.Text;
+            colour = colourInput.Text;
+            year = Convert.ToInt16(yearInput.Text);
+            mileage = Convert.ToInt32(mileageInput.Text);
+
+            Car c = new Car(make, colour, year, mileage);
+            cars.Add(c);
+
+            outputLabel.Text = "";
+
+            for (int i = 0; i < cars.Count; i++)
+            {
+                outputLabel.Text += cars[i].make + " " +
+                    cars[i].year + " " +
+                    cars[i].colour + " " +
+                    cars[i].mileage + "\n";
+            }
+
+            makeInput.Text = colourInput.Text = yearInput.Text = mileageInput.Text = "";
+            yearInput.Focus();
+        }
+    }
+}
